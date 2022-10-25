e47644571341117e091b687ddc67bdd6757e2c1f
Little Lin <82437681+3829388vs@users.noreply.github.com>
2022-10-25 16:35:00 +0800

    Delete src directory

--- src/uitls/util.js
+++
@@ -1,8 +0,0 @@
-// 主要用于echarts  px转rem 参数res是传已转rem的
-export const fontSize = (res) => {
-    let docEl = document.documentElement,
-        clientWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    if (!clientWidth) return;
-    let fontSize = 100 * (clientWidth / 1920);
-    return res * fontSize;
-}
